ea211bec02f7b8f39f68c9d4f6becca585ca9734
surayut <48819562+surayut99@users.noreply.github.com>
2023-11-22 21:23:00 +0700

    Update/git module (#2)
    * remove unused file
    
    * Update git module

--- Modules/GitHelper/GitHelper.psm1
+++ Modules/GitHelper/GitHelper.psm1
@@ -1,9 +1,75 @@
 Import-Module (Join-Path $PSScriptRoot "GitValidator.psm1") -Force
 
-function gph {
+function gstat {
+    git status
+}
+
+function glog {
+    param (
+        [switch]$b
+    )
+
+    if ($b) {
+        git log --all --decorate --oneline --graph
+    } else {
+        git log
+    }
+    
+}
+
+function greset {
+    param (
+        [int] $count,
+        [switch] $h
+    )
+
+    if ($h) {
+        git reset --hard HEAD~$count
+    }
+    else {
+        git reset --soft HEAD~$count
+    }
+    
+}
+
+
+function gcommit {
     [CmdletBinding()]
     param (
         [string]$commitMessage,
+        [switch]$a
+    )
+
+    if ([string]::IsNullOrEmpty($commitMessage)) {
+        Write-Error "Commit message is required."
+        return
+    }
+
+    ValidateGitDirectory
+
+    Write-Host "Adding files to commit..."
+    git add .
+
+    Write-Host "Commiting changes..."
+    git commit -m $commitMessage
+
+    # Perform git push origin <current branch> (with force if -f switch is provided)
+    if ($a) {
+        Write-Host "Amending previous commit message..."
+        git commit --amend -m $commitMessage
+    }
+    else {
+        git commit -m $commitMessage
+    }
+
+    Write-Host "Add -> Commit successfully"
+}
+
+function gpush {
+    [CmdletBinding()]
+    param (
+        [string]$commitMessage,
+        [string]$origin = "origin",
         [switch]$f
     )
 
@@ -14,11 +80,8 @@ function gph {
 
     ValidateGitDirectory
 
-    # # Check if git is installed
-    # if (-Not (Get-Command git -ErrorAction SilentlyContinue)) {
-    #     Write-Error "Git is not installed. Please install Git before using this function."
-    #     return
-    # }
+    # Get the current branch name
+    $branch = git rev-parse --abbrev-ref HEAD
 
     Write-Host "Adding files to commit ..."
     git add .
@@ -26,27 +89,25 @@ function gph {
     Write-Host "Commiting changes ..."
     git commit -m $commitMessage
 
-    # Get the current branch name
-    $branch = git rev-parse --abbrev-ref HEAD
-
     Write-Host "Pushing the commit to branch: $branch ..." 
 
     # Perform git push origin <current branch> (with force if -f switch is provided)
     if ($f) {
         Write-Host "FORCE Pushing ..."
-        git push --force origin $branch
-    } else {
-        git push origin $branch
+        git push --force $origin $branch
+    }
+    else {
+        git push $origin $branch
     }
 
     Write-Host "Add -> Commit -> Push to $branch successfully"
 }
 
-function gcb {
+function gco {
     [CmdletBinding()]
     param (
-        # [Parameter(Mandatory=$true)]
         [string]$branchName,
+        [switch]$r,
         [string]$safeBranch = "master"
     )
     if ([string]::IsNullOrEmpty($branchName)) {
@@ -56,40 +117,29 @@ function gcb {
 
     ValidateGitDirectory
 
-    # # Check if git is installed
-    # if (-Not (Get-Command git -ErrorAction SilentlyContinue)) {
-    #     Write-Host "Git is not installed. Please install Git before using this function."
-    #     return
-    # }
-
-    # # Check if the current directory is a git repository
-    # if (-Not (Test-Path -PathType Container (Join-Path $_ ".git"))) {
-    #     Write-Error "The current directory is not a git repository. Please run 'git init' first or change to a valid git repository directory."
-    #     return
-    # }
-    
-
     # Get the current branch name
     Write-Host "Checking current branch ..."
     $currentBranch = git rev-parse --abbrev-ref HEAD
 
-    # Check if the current branch is the one we want to delete
-    if ($currentBranch -eq $branchName) {
-        # Checkout the safe branch before deleting the target branch
-        Write-Host "Check out to safe branch: $safeBranch"
-        git checkout $SafeBranch
+    # Re-create branch
+    if ($r) {
+        # Check if the current branch is the one we want to delete
+        if ($currentBranch -eq $branchName) {
+            # Checkout the safe branch before deleting the target branch
+            Write-Host "Check out to safe branch: $safeBranch"
+            git checkout $SafeBranch
+            git branch -D $branchName
+            git checkout -b $branchName
+            Write-Host "Re-create branch: $safeBranch successfully"
+            return;
+        }
     }
 
-    # Check if the branch exists locally
-    $localBranches = git branch
-    if ($localBranches -match "\b$branchName\b") {
-        # The branch already exists locally, so force delete it
-        git branch -D $branchName
-        Write-Host "Delete out-date branch: $branchName"
+    git checkout $branchName 2>null | Invoke-Expression
+    $currentBranch = git rev-parse --abbrev-ref HEAD
+
+    if (!($currentBranch -eq $branchName)) {
+        git checkout -b $branchName
     }
 
-    # Create and checkout the new branch
-    git checkout -b $branchName
-
-    Write-Host "Create and check out a new branch successfully"
 }
