12fd4ecd1c841903e9b04925f32199f011460e9a
BubbleShot <surayut.boonklay@gmail.com>
2023-11-22 21:18:08 +0700

    Update git module

--- Modules/GitHelper/GitValidator.psm1
+++ Modules/GitHelper/GitValidator.psm1
@@ -7,7 +7,7 @@ function ValidateGitDirectory {
     }
 
     # Check if the current directory is a git repository
-    if (-Not (Test-Path -PathType Container (Join-Path $_ ".git"))) {
+    if (-Not (Test-Path -PathType Container (Join-Path $PWD ".git"))) {
         Write-Error "The current directory is not a git repository. Please run 'git init' first or change to a valid git repository directory."
         return
     }
